8b5e65ab89f54699ea0aac6e6bd7a76d0364d8c2
codemonk8 <153008199+codemonk8@users.noreply.github.com>
2025-11-16 11:38:18 +0100

    Revert "delete imports"
    This reverts commit d3e3eb71331134c183caf5939c688317ea8c76b7.

--- src/pages/LearningMode.tsx
+++ src/pages/LearningMode.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
+// import { supabase } from "@/integrations/supabase/client";
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { Progress } from "@/components/ui/progress";
